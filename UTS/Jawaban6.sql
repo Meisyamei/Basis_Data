1fa4df2cd8d5b720703f30079992beba34d2712e
Meisyamei <146688835+Meisyamei@users.noreply.github.com>
2023-11-16 21:44:00 +0700

    Update Jawaban6.sql

--- UTS/Jawaban6.sql
+++ UTS/Jawaban6.sql
@@ -1,4 +1,4 @@
-///MENAMPILKAN DATA MAHASISWA, DOSEN DAN MATA KULIAH///
+-- MENAMPILKAN DATA MAHASISWA, DOSEN DAN MATA KULIAH
   
 SELECT CONCAT_WS(' ', m.first_name, m.last_name) as `Mahasiswa`, k.`matkul` as `Mata_Kuliah`, CONCAT_WS(' ', d.first_name, d.last_name) as `Dosen`
 FROM Mahasiswa as m
